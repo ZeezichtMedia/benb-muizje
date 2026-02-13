c1430e3db6a36412470df0d4d3971b2839841b42
Ruben Boogaard <rubenboogaard@MacBook-Air-van-Ruben.local>
2026-02-13 14:11:34 +0100

    fix: force output:'static' to resolve vercel 500 errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
